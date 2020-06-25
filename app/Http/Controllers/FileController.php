4dc3f34fdbcbc87245757540284af0df21b216c2
oks77 <oksanastavenko@gmail.com>
2020-06-25 13:18:11 +0300

    first commit

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,26 @@
+<?php
+//
+//namespace App\Http\Controllers;
+//
+//use Illuminate\Http\Request;
+//
+//class FileController extends Controller
+//{
+//
+//}
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileController extends Controller
+{
+    public function store(Request $request)
+    {
+        $fileName = time() . '.' . $request->file->getClientOriginalExtension();
+        $request->file->move(public_path('files'), $fileName);
+
+        return response()->json(['file' => $fileName]);
+    }
+}
